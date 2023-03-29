3bc60afe77b50a3ad364ef5dc1907d49d8a2e72a
Messai <messai@github.com>
2023-03-29 14:53:31 +0300

    Added 118

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,11 +10,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (node == NULL || node->parent == NULL)
-                return (NULL);
-        if (node->parent->left == node)
-                return (node->parent->right);
-        return (node->parent->left);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
 
 
