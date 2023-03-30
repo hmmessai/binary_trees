fb653265810d8b0d5dc9510e9a64773a5b0e9d6a
Messai <messai@github.com>
2023-03-30 18:46:44 +0300

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newLeft = binary_tree_node(parent, value);
-	if (!new)
+	if (!newLeft)
 		return (NULL);
 
 	if (parent->left)
